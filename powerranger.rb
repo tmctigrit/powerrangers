a52dacf4135bd6d2cc6ce87971778890879f9e8c
tmctigrit <mctigrit.tricia@gmail.com>
2014-11-06 20:30:06 -0600

    Added attributes and methods to Person class
    Added name & caffeine_level attributes.  Added run, scream, and
    drink_coffee methods.

--- powerranger.rb
+++ powerranger.rb
@@ -1,8 +1,38 @@
 class Person
+
+  def initialize(name, caffeine_level)
+    @name = name.capitalize
+    @caffeine_level = caffeine_level
+  end
+
+  def run(miles)
+    puts "#{@name} ran #{miles} miles."
+  end
+
+  def scream(noise)
+    puts "#{noise.upcase}!!!!"
+  end
+
+  def drink_coffee(cups)
+    @caffeine_level += cups
+    puts "#{@name}'s caffeine level is #{@caffeine_level}.  Yowza!"
+  end
+
 end
+
 
 class PowerRanger
 end
 
 class EvilNinja
 end
+
+person1 = Person.new("trish", 4)
+
+person1.run(10)
+
+person1.scream("ahhh")
+
+person1.drink_coffee(4)
+
+person1.drink_coffee(4)
