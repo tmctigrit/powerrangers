b3411352c054b8cef24ae1268bbf27bfa0b3485c
tmctigrit <mctigrit.tricia@gmail.com>
2014-11-15 16:33:41 -0600

    PowerRangers App - MKS

--- powerranger.rb
+++ powerranger.rb
@@ -1,38 +1,96 @@
-class Person
+module Punching
+    def punch(person)
+    if @strength > 5
+      puts "#{person} goes flying into the air!  You packed a mean punch!"
+      @caffeine_level -= 2
+    elsif @strength <= 5
+      puts "#{person} screams and runs away! What a baby!"
+      @caffeine_level -= 2
+    end
+  end
+end
 
-  def initialize(name, caffeine_level)
+
+class Person
+  attr_accessor :name, :caffeine_level
+  def initialize(name, caffeine_level=0)
     @name = name.capitalize
     @caffeine_level = caffeine_level
   end
 
   def run(miles)
-    puts "#{@name} ran #{miles} miles."
+    if @caffeine_level < 2
+      puts "Phenin' for caffeine!  You need more coffee before you can run!"
+    else
+      puts "It's power run time!  You run #{miles} miles!"
+    end
   end
 
   def scream(noise)
-    puts "#{noise.upcase}!!!!"
+    if @caffeine_level < 2
+      puts "Not enough energy.  Get yourself an espresso shot!"
+    else
+      puts "AHHHHHHHHH!!!!!!!!"
+    end
   end
 
   def drink_coffee(cups)
     @caffeine_level += cups
-    puts "#{@name}'s caffeine level is #{@caffeine_level}.  Yowza!"
+    puts "Your caffeine level is #{@caffeine_level}.  Yowza!"
   end
 
 end
 
 
-class PowerRanger
+class PowerRanger < Person
+  include Punching
+  attr_accessor :name, :caffeine_level, :strength, :color
+  def initialize(name, caffeine_level, strength, color)
+    super(name, caffeine_level)
+    @strength = strength
+    @color = color
+  end
+
+  def rest
+    @strength += 2
+    puts "You awake feeling stronger and refreshed.  It's Ranger time!"
+  end
+
+  def use_megazord
+    puts "KABOOM!  You served up a megazord punch with a strength of 5000!  Strength level has been reset to 1."
+    @strength = 1
+  end
+
 end
 
-class EvilNinja
+class EvilNinja < Person
+  include Punching
+  attr_accessor :name, :caffeine_level, :strength, :evil_level
+
+  def initialize(name, caffeine_level, strength, evil_level)
+    super(name, caffeine_level)
+    @strength = strength
+    @evil_level = evil_level
+  end
+
+  def cause_mayhem(person)
+    @caffeine_level = 0
+    puts "You've caused mayhem! #{person.name}'s caffeine level has been reduced to #{@caffeine_level}.  Terrifying!"
+  end
+
+  def fight_scene
+
+  end
+
 end
 
-person1 = Person.new("trish", 4)
+trish = Person.new("Trish",8)
+jenn = Person.new("Jenn", 3)
+pink = PowerRanger.new("Pink Ranger", 2, 3, "pink")
+blue = PowerRanger.new("Blue Ranger", 3, 4, "royal blue")
+matsumuro = EvilNinja.new("Matsumuro", 5, 50, 10)
+rafiki = EvilNinja.new("Rafiki", 3, 20, 50)
 
-person1.run(10)
 
-person1.scream("ahhh")
 
-person1.drink_coffee(4)
 
-person1.drink_coffee(4)
